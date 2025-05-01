67f50bca4fc012035354380f6361a70394900771
isaac-nkansah <nkansahisaac41@gmail.com>
2025-05-01 01:23:28 +0000

    fixes

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -20,15 +20,8 @@ class UserController {
       final response = await http.patch(
           Uri.parse(
               'https://fixit-testing.tuulbox.app/api/accounts/6d475484-c5d6-492d-98c7-27b0733806b1/'),
-          headers: {
-            "Authorization":
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2NjI5NzE4LCJpYXQiOjE3NDYwMjQ5MTgsImp0aSI6ImZmNmI0YWM4ODlkYzQzYzdhYmEyMzQyZGE0ZjY2NGMxIiwidXNlcl9pZCI6IjZkNDc1NDg0LWM1ZDYtNDkyZC05OGM3LTI3YjA3MzM4MDZiMSJ9.rkyTGLSyeY6pGQMWoTrjPq4uMYg5No7L39Cmmr_bCIA"
-          },
-          body: {
-            "firstName": firstName,
-            "lastName": lastName,
-            "address": address
-          });
+          headers: {"Authorization": "Bearer <token>"},
+          body: {"firstName": firstName, "lastName": lastName, "address": address});
       setLoadingState = false;
       if (response.statusCode == 200) {
         return (true, "User profile updated successfully");
